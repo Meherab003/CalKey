38bd68ab3f10a0f2ef7436d50d53683334cfbf1f
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-02-16 15:23:12 +0600

    Fully Finctioned

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 const display = document.getElementById("display");
+
+function keyPad(keyValue) {
+  const currentValue = display.value;
+  const newValue = keyValue;
+  display.value = currentValue + newValue;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteNumber() {
+  if (display.value) {
+    const keyValue = display.value.split("");
+    keyValue.pop();
+    display.value = keyValue.join("");
+  }
+}
+
+function solution() {
+  if (display.value) {
+    display.value = eval(display.value);
+  } else {
+    alert("You haven't typed anything, have you?");
+  }
+}
